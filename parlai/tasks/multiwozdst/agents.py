107e80dceede71bfe287aeab4a6c0fc9008bbc13
Kun Qian <kunqian@devfair036.maas>
2020-08-12 00:44:21 -0700

    add joint goal acc in custom_evaluation

--- parlai/tasks/multiwozdst/agents.py
+++ parlai/tasks/multiwozdst/agents.py
@@ -4,6 +4,8 @@ import sys, os
 import json
 
 from parlai.core.teachers import FixedDialogTeacher
+from parlai.core.message import Message
+from parlai.core.metrics import AverageMetric
 from .build import build
 from .utils.trade_proc import trade_process
 from .utils.reformat import reformat_parlai
@@ -67,8 +69,66 @@ class MultiWozDSTTeacher(FixedDialogTeacher):
             train_path = data_path.replace(".json", "_train.json")
             train_data = self._load_json(train_path)
             self.messages = list(train_data.values())
+    
 
-    # def customize_evaluation(self)
+    def _extract_slot_from_string(self, slots_string):
+        """
+        Either ground truth or generated result should be in the format:
+        "dom slot_type slot_val, dom slot_type slot_val, ..., dom slot_type slot_val,"
+        and this function would reformat the string into list:
+        ["dom--slot_type--slot_val", ... ]
+        """
+        domains    = ["attraction", "hotel", "hospital", "restaurant", "police", "taxi", "train"]
+        slot_types = ['book time', 'leaveat', 'name', 'internet', 'book stay', 
+                      'pricerange', 'arriveby', 'area', 'destination', 'day', 
+                      'food', 'departure', 'book day', 'book people', 'department', 
+                      'stars', 'parking', 'type']
+        slots_list = []
+
+        # # # remove start and ending token
+        str_split = slots_string.strip().split()
+        if str_split != [] and str_split[0] in ["<bs>", "</bs>"]:
+            str_split = str_split[1:]
+        if "</bs>" in str_split:
+            str_split = str_split[:str_split.index("</bs>")]
+
+        # # # split according to ","
+        str_split = " ".join(str_split).split(",")
+        if str_split[-1] == "":
+            str_split = str_split[:-1]
+        str_split = [slot.strip() for slot in str_split]
+
+        for slot_ in str_split:
+            slot = slot_.split()
+            if len(slot) > 2 and slot[0] in domains:
+                domain = slot[0]
+                if slot[1] == "book" and slot[2] in ["day", "time", "people", "stay"]:
+                    slot_type = slot[1]+" "+slot[2]
+                    slot_val  = " ".join(slot[3:])
+                else:
+                    slot_type = slot[1]
+                    slot_val  = " ".join(slot[2:])
+                slots_list.append(domain+"--"+slot_type+"--"+slot_val)
+        return slots_list
+
+
+    def custom_evaluation(self, teacher_action: Message, labels, model_response: Message):
+        resp = model_response.get('text')
+        if not resp:
+            return
+
+        # # # extract ground truth from labels
+        slots_truth = self._extract_slot_from_string(labels[0])
+        
+        # # # extract generated slots from model_response
+        slots_pred = self._extract_slot_from_string(resp)
+
+        self.metrics.add('joint goal acc', AverageMetric(set(slots_truth) == set(slots_pred)))
+
+        # if set(slots_truth) == set(slots_pred):
+        #     self.metrics.add('joint goal acc', AverageMetric(1))
+        # else:
+        #     self.metrics.add('joint goal acc', AverageMetric(0))
 
     def num_examples(self):
         # each turn be seen as a individual dialog
